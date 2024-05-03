23995ed80da213eb16a96a8a6044b19051993869
Kai Broadbent <appblazarknight@gmail.com>
2024-05-02 20:17:08 -0400

    add can openers

--- formater/format.py
+++ formater/format.py
@@ -1,8 +1,14 @@
 def can_opener_pdf(file):
+    from PyPDF2 import PdfReader
+
+    reader = PdfReader(file)
+    page = reader.pages[0]
+    extracted_text = page.extract_text()
+    return extracted_text
 
 
 def clean(input:str,extent:str):
-
+    return can_opener_pdf(input)
 
 
 
@@ -13,4 +19,4 @@ def clean(input:str,extent:str):
 
 if __name__ == "__main__":
     
-    print(clean())
+    print(clean('leggett_v._the_sanctuary_at_false_cape_condo._assn._order.pdf','pdf'))
