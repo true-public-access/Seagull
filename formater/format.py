e670ab623cdba8321c30438986cfdcb0283455f1
Kai Broadbent <appblazarknight@gmail.com>
2024-05-02 20:11:36 -0400

    worked on canopeners

--- formater/format.py
+++ formater/format.py
@@ -1,18 +1,8 @@
-def get_fileexturl(url):
-    '''from urllib.parse import urlparse
-    import os 
-    path = urlparse(url).path
-    path_without_params, _ = os.path.splitext(path.split('?')[0])
-    _, file_extension = os.path.splitext(path_without_params)
-    return file_extension'''
-    import re
-    match = re.search(r'\.([a-zA-Z0-9]+)$', url)
-    if match:
-        return match.group(1)
-    else:
-        return None
-    
+def can_opener_pdf(file):
+
+
 def clean(input:str,extent:str):
+
 
 
 
@@ -23,4 +13,4 @@ def clean(input:str,extent:str):
 
 if __name__ == "__main__":
     
-    print(clean("div>   <p>       Some text       <span>more text</span>       even more text   </p>   <ul>       <li>list item</li>       <li>yet another list item</li></ul></div><p>Some other text</p><ul>    <li>list item</li><li>yet another list item</li></ul>"))
+    print(clean())
