9f2dcc898ed3632a18089f19683694ce1e1c89aa
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2024-05-03 18:09:09 +0000

    trying out gui moduals

--- startup.py
+++ startup.py
@@ -1,6 +1,19 @@
-import tkinter
-m = tkinter.Tk()
-'''
-widgets are added here
-'''
-m.mainloop()
+from kivy.app import App
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+
+class TutorialApp(App):
+    def build(self):
+        f = FloatLayout() #ignore error
+        s = Scatter()
+        l = Label(text="Hello!",
+                  font_size=150)
+
+        f.add_widget(s)
+        s.add_widget(l)
+        return f
+
+if __name__ == "__main__":
+    TutorialApp().run()
