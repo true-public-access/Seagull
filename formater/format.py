a92577cb77b6444610776c4909136f7413de4e65
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2024-05-06 16:36:15 +0000

    continued to refine startup notices

--- formater/format.py
+++ formater/format.py
@@ -1,3 +1,19 @@
+''' Seagull format this file defines the funtions that cleans/formats the data form the internet, 
+    Copyright (C) 2024 Kai Broadbent 'BlazarKnight'
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to appblazarknight@gmail.com
+'''
 def can_opener_pdf(file):
     from PyPDF2 import PdfReader
 
--- startup.py
+++ startup.py
@@ -1,15 +1,44 @@
-import flet as ft
+''' Seagull statup this file is run at the start of the program to outline terms and conditions, 
+    Copyright (C) 2024 Kai Broadbent 'BlazarKnight'
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to appblazarknight@gmail.com
+    '''
+
+
+
+
+
+import flet as ft
+import os
 def main(page):
+    
+    
+    
+    
+    
+    
+    
     def close_banner(e):
         page.banner.open = False
         page.update()
 
-    def show_w():
-        return None
+    def show_w(e):
+        worent.open = True
+        worent.update()
     
-    def show_c():
-        
+    def show_c(e):
+        return None
 
 
 
@@ -19,14 +48,45 @@ def main(page):
         content=ft.Text("Seagull version 1, Copyright (C) 2024 Kai Broadbent 'BlazarKnight' Seagull comes with ABSOLUTELY NO WARRANTY; for details hit `show w'. This is free software, and you are welcome to redistribute it under certain conditions; hit `show c' for details.", color=ft.colors.RED,size=40),
         actions=[
             ft.TextButton("show w", on_click=show_w),
-            ft.TextButton("show c", on_click=close_banner),
+            ft.TextButton("show c", on_click=show_c),
             ft.TextButton("I understand and accept", on_click=close_banner),
         ],
-    )
+        )
 
     def show_banner_click(e):
         page.banner.open = True
         page.update()
+    
+
+# bottum baner stuff
+
+    def bs_dismissed(e):
+        print("Dismissed!")
+
+    def show_bs(e):
+        worent.open = True
+        worent.update()
+
+    def close_bs(e):
+        worent.open = False
+        worent.update()
+
+    worent= ft.BottomSheet(
+        ft.Container(
+            ft.Column(
+                [
+                    ft.Text(os.open('/workspaces/Seagull/text_to_display/warrantyfordis').text),
+                    ft.ElevatedButton("Close and accept", on_click=close_bs),
+                ],
+                tight=True,
+            ),
+            padding=10,
+        ),
+        open=False,
+        on_dismiss=bs_dismissed,
+        )
+    page.overlay.append(worent)
+    
 
     show_banner_click(1)
 
