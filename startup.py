0191c46a45aa2c11ef100bba0b161e9b78c6dfa0
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2024-05-08 12:53:26 +0000

    fixed typo

--- startup.py
+++ startup.py
@@ -46,7 +46,7 @@ def main(page):
     page.banner = ft.Banner(
         bgcolor=ft.colors.AMBER_100,
         leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.AMBER, size=80),
-        content=ft.Text("Seagull version 1, Copyright (C) 2024 Kai Broadbent 'BlazarKnight' Seagull comes with AwarenOLUTELY NO WARRANTY; for details hit `show w'. This is free software, and you are welcome to redistribute it under certain conditions; hit `show c' for details.", color=ft.colors.RED,size=40),
+        content=ft.Text("Seagull version 1, Copyright (C) 2024 Kai Broadbent 'BlazarKnight' Seagull comes with ABSOLUTELY NO WARRANTY; for details hit `show w'. This is free software, and you are welcome to redistribute it under certain conditions; hit `show c' for details.", color=ft.colors.RED,size=40),
         actions=[
             ft.TextButton("show w", on_click=show_w),
             ft.TextButton("show c", on_click=show_c),
