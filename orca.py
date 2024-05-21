05219375c576cd7816d9e6ef89d1e01cf61284c4
BlazarKnight <152732605+BlazarKnight@users.noreply.github.com>
2024-05-21 07:22:27 -0400

    Set up stuff (#60)
    * Pending changes exported from your codespace
    
    * wored on sec atomat and front

--- orca.py
+++ orca.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     from formater.format import clean
 
     raw=geturlsorce("https://www.sec.gov/Archives/edgar/data/38777/000003877724000070/frkbsp24a6.htm")
-    print(clean(raw))
+    print(clean(raw,"htm"))
     
 
 
